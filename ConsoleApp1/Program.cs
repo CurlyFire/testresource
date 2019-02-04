9cccac7826000b82c4ada226c43450ad0e1ea395
CurlyFire <stephane.denomme@inspq.qc.ca>
2019-02-04 13:27:44 -0500

    test

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var res = Resource1.ResourceManager.GetObject("avatar");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
